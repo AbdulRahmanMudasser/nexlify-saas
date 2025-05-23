be6577710a9a3da16430bd8206bb29f3f45c1809
Abdul Rahman <102192151+AbdulRahmanMudasser@users.noreply.github.com>
2025-05-23 23:09:36 +0500

    shadecn bug resolved, build is running

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,11 @@
+import { Button } from '@/components/ui/button'
 import React from 'react'
 
 const Page = () => {
   return (
     <div>
       <h1 className='text-2xl underline'>Welcome to Nexlify</h1>
+      <Button variant="black">Let's Get Started</Button>
     </div>
   )
 }
