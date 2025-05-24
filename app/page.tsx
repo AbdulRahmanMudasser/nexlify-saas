7e192320cf298b38d0d2eae5224ff2374bd3b581
Abdul Rahman <102192151+AbdulRahmanMudasser@users.noreply.github.com>
2025-05-24 16:04:17 +0500

    added companion cards and section, configured page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,50 @@
-import { Button } from '@/components/ui/button'
+import CompanionCard from '@/components/CompanionCard'
+import CompanionsList from '@/components/CompanionsList'
+import CTA from '@/components/CTA'
 import React from 'react'
 
 const Page = () => {
   return (
-    <div>
-      <h1 className='text-2xl underline'>Welcome to Nexlify</h1>
-      <Button variant="black">Let's Get Started</Button>
-    </div>
+    <main>
+      <h1 className='text-2xl'>Popular Companions</h1>
+
+      {/* Companion Cards */}
+      <section className='home-section'>
+        <CompanionCard
+          id='123'
+          name='Neura the Brainy Explorer'
+          topic='Neural Network of the Brain'
+          subject='Science'
+          duration={45}
+          color='#BDE7FF'
+        />
+
+        <CompanionCard
+          id='456'
+          name='Countsy the Number Wizard'
+          topic='Derivatives & Integrals'
+          subject='Maths'
+          duration={30}
+          color='#FFDA6E'
+        />
+
+        <CompanionCard
+          id='789'
+          name='Verba the Vocabulary Builder'
+          topic='English Literature '
+          subject='Language'
+          duration={45}
+          color='#E5D0FF'
+        />
+      </section>
+
+      {/* Companions List */}
+      <section className='home-section'>
+        <CompanionsList />
+        <CTA />
+      </section>
+
+    </main>
   )
 }
 
